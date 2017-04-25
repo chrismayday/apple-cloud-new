e65b7a22b2954e11dced4f50794ba6cec5f85183
tangmin721 <191102902@qq.com>
2017-04-25 22:56:33 +0800

    加入TEST

--- apple-cloud-consumer-user/src/main/java/com/cachexic/apple/consumer/WEBMessageConvert.java
+++ apple-cloud-consumer-user/src/main/java/com/cachexic/apple/consumer/WEBMessageConvert.java
@@ -2,10 +2,17 @@ package com.cachexic.apple.consumer;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.http.MediaType.TEXT_HTML;
 
 /**
  * @author tangmin
@@ -33,6 +40,10 @@ public class WEBMessageConvert {
     public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(){
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(objectMapper);
+        List<MediaType> supportedMediaTypes = Lists.newArrayList();
+        supportedMediaTypes.add(APPLICATION_JSON_UTF8);
+        supportedMediaTypes.add(TEXT_HTML);
+        converter.setSupportedMediaTypes(supportedMediaTypes);
         return converter;
     }
 
